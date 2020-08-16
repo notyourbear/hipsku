e334ef906fc5b4e3a0eb0e0517678ab7399d12a3
Patrick Racenberg <pracenberg@gmail.com>
2020-08-16 11:49:16 -0700

    works

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,9 +1,59 @@
+import syllable from "syllable";
 import markovChain from "./markov";
+import { sample } from "./utils";
 import api from "../api";
+
+const LINECOUNT = [3, 5, 3];
+
+export const pickWord = (state = {}, syllableCount = 1) => {
+  let words = Object.keys(state);
+  let word = words[0];
+
+  while (syllable(word) !== syllableCount) {
+    let { index, value } = sample(words);
+    words = [...words.slice(0, index), ...words.slice(index + 1)];
+    word = value;
+  }
+
+  return word;
+};
+
+export const generateLine = (state = {}, syllableCount = 3) => {
+  const stateValues = Object.values(state);
+
+  let count = 0;
+  let phrase = "";
+
+  while (count < syllableCount) {
+    let lineOptions = sample(stateValues);
+    const { value } = sample(lineOptions.value);
+    console.log({ lineOptions, value });
+
+    const addedSyllables = syllable(value);
+    if (count + addedSyllables <= syllableCount) {
+      count += addedSyllables;
+      phrase = `${value} ${phrase}`;
+    }
+
+    if (syllableCount - count === 1) {
+      const word = pickWord(state);
+      count += 1;
+      phrase = `${phrase}${word}`;
+    }
+  }
+
+  return phrase.trim();
+};
 
 const generate = () => {
   return api.fetchData(50).then((data) => {
-    const state = markovChain(data);
+    const state = markovChain(data, 1);
+    const line = generateLine(state);
+    const poem = LINECOUNT.reduce(
+      (acc, count) => [...acc, generateLine(state, count)],
+      []
+    );
+    console.log({ poem });
     return state;
   });
 };
---
+++ src/generator/utils.js
@@ -0,0 +1,17 @@
+export const randomInt = (min = 0, max = 1) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const sample = (array = []) => {
+  const min = 0;
+  const max = array.length === 1 ? 0 : array.length;
+  const index = randomInt(min, max);
+
+  return {
+    value: array[index],
+    index,
+  };
+};
+
+export default {
+  sample,
+};
